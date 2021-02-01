c707ae2df6f439454528f77a1b86fd23a5408156
Jon Ambas <jon@jonambas.com>
2021-02-01 18:33:02 -0500

    Add build workflow, Update package information (#2)
    * Remove jon ambas
    
    * Add npm link scripts, Update package information
    
    * Update repo name
    
    * Add build workflow
    
    * Replace example imports with @sparkpost/libby-react
    
    * Update workflow job name
    
    * Update workflow script, package name
    
    * Update workflow script

--- example/src/test2.libby.js
+++ example/src/test2.libby.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { add, describe } from 'libby-react';
+import { add, describe } from '@sparkpost/libby-react';
 
 describe('Error', () => {
   add('renders correctly', () => <div>This is {oops} a React component</div>);
