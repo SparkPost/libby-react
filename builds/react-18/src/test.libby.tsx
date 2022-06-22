a0b96cb9a5af6f797f984f9e7c9ef1492b6ec591
Jon Ambas <jon@jonambas.com>
2022-06-21 22:02:38 -0400

    build: Add 17 & 18 workflows

---
+++ builds/react-18/src/test.libby.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { add, describe } from '@sparkpost/libby-react';
+
+const Component: React.FC<{ children?: React.ReactNode }> = (props): JSX.Element => {
+  return <div {...props} />
+}
+
+describe('TSX', () => {
+  add('renders correctly', () => <Component>This is a React component with typescript</Component>);
+});
--- example/src/Image/Image.libby.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { describe, add } from '@sparkpost/libby-react';
-import Image from './Image';
-
-describe('Image', () => {
-  add('renders correctly', () => {
-    return <Image />;
-  });
-});
